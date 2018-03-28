aeec46d8a80bec7845c7089bab6de818626af3ea
juliennuellas <julien.nuellas@gmail.com>
2018-03-28 14:45:18 +0200

    Midification fichiers : mise à jour fichier core et dbcreation

---
+++ constants.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+MIN_PRODUCT_NUMB = 1000
+MAX_PRODUCT_NUMB = 5000
+MAX_CATEGORY = 2
---
+++ core.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+from dbcreation import DatabaseCreation
+import argparse
+import password as pw
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--database', help="""Action on Database. Type
+    create to create and feed the database. Type update to force the update of
+    the database""")
+    return parser.parse_args()
+
+def main():
+    args = parse_arguments()
+    if args.database == 'create':
+        database = DatabaseCreation(pw.HOST, pw.USERNAME, pw.PASSWORD, pw.DATABASE)
+        database.prepare()
+    print('READY TO PLAY')
+
+if __name__ == "__main__":
+    main()
